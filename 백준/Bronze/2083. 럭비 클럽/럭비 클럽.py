29779b3d32ca7c56142b69ebccff712400643120
Yu NaHyun <117415639+12xii@users.noreply.github.com>
2023-11-24 08:26:34 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.py
@@ -0,0 +1,5 @@
+while True:
+    member = input().split()
+    if(member[0] == '#'): break
+    if(int(member[1]) > 17 or int(member[2]) >= 80): print(f'{member[0]} Senior')
+    else: print(f'{member[0]} Junior')
